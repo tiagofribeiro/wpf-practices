7ddf73ed66f771c9a5e42966d07d2f491051bd39
tiagofribeiro <tiagofr98@gmail.com>
2023-07-11 02:28:47 -0300

    basic operations

--- WPF-Basics/Calculator/MainWindow.xaml.cs
+++ WPF-Basics/Calculator/MainWindow.xaml.cs
@@ -20,11 +20,80 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double _lastNumber, _result;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            resultLabel.Content = "1";
+            clearButton.Click += ClearButton_Click;
+            negaButton.Click += NegaButton_Click;
+            percentButton.Click += PercentButton_Click;
+            equalButton.Click += EqualButton_Click;
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OperationHandler(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out _lastNumber))
+            {
+                _lastNumber /= 100;
+                resultLabel.Content = _lastNumber.ToString();
+            }
+        }
+
+        private void NegaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out _lastNumber))
+            {
+                _lastNumber *= -1;
+                resultLabel.Content = _lastNumber.ToString();
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void NumberHandler(object sender, RoutedEventArgs e)
+        {
+            int selectedNumber = 0;
+
+            if (sender == zeroButton)
+                selectedNumber = 0;
+            if (sender == oneButton)
+                selectedNumber = 1;
+            if (sender == twoButton)
+                selectedNumber = 2;
+            if (sender == threeButton)
+                selectedNumber = 3;
+            if (sender == fourButton)
+                selectedNumber = 4;
+            if (sender == fiveButton)
+                selectedNumber = 5;
+            if (sender == sixButton)
+                selectedNumber = 6;
+            if (sender == sevenButton)
+                selectedNumber = 7;
+            if (sender == eightButton)
+                selectedNumber = 8;
+            if (sender == nineButton)
+                selectedNumber = 9;
+
+            if (resultLabel.Content.ToString() == "0")
+                resultLabel.Content = $"{selectedNumber}";
+            else 
+                resultLabel.Content = $"{resultLabel.Content}{selectedNumber}";
         }
     }
 }
