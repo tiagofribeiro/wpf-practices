d8aa94246d78c497cd1565491e342c4d0068f03e
tiagofribeiro <tiagofr98@gmail.com>
2023-07-12 02:46:51 -0300

    classes

---
+++ WPF-Basics/Fundamentals/Program.cs
@@ -0,0 +1,9 @@
+﻿using Fundamentals.Classes;
+
+// Do .NET 6 em diante, o compilador sintetiza uma classe Program com um método Main e coloca todas as instruções de nível superior nesse método.
+
+Console.WriteLine("Início!");
+
+BankAccount bankAccount = new BankAccount();
+
+Console.WriteLine(bankAccount.AddToBalance(20000));
