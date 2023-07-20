7a04d527322c37216242dedbf76bce767c1eaea0
tiagofribeiro <tiagofr98@gmail.com>
2023-07-20 18:00:50 -0300

    :hash: MVVM
    model and viewmodel

---
+++ WPF-Intermediary/WeatherApp/ViewModel/AccuWeatherHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    internal class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "/locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENTCONDITION_ENDPOINT = "/currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "uvkLz0LpYl2roY6rGGfGGtalTzSAAJ3G";
+
+        public static List<City> GetCities(string query)
+        {
+            List<City> cityList = new();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using (HttpClient client = new()) 
+            {
+                //Fazer request
+            }
+
+            return cityList;
+        }
+    }
+}
