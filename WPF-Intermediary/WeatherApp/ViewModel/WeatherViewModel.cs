dc04b3105d1e38905ae6901ed824587902b2b85d
tiagofribeiro <tiagofr98@gmail.com>
2023-07-27 17:47:53 -0300

    INotificationPropertyChanged implemented

---
+++ WPF-Intermediary/WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    class WeatherViewModel : INotifyPropertyChanged
+    {
+        // Implementação de interfaces
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Propriedades privadas
+        private int query;
+        private CurrentConditions currentConditions;
+        private City selectedcity;
+
+        // Propriedades públicas
+        public int Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged(nameof(CurrentConditions));
+            }
+        }
+
+        public City SelectedCity
+        {
+            get { return selectedcity; }
+            set
+            {
+                selectedcity = value;
+                OnPropertyChanged(nameof(SelectedCity));
+            }
+        }
+
+        public WeatherViewModel()
+        {
+            // Cria dados "mockados" apenas no designer
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "Belo Horizonte"
+                };
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Nublado",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 30,
+                        }
+                    }
+                };
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
